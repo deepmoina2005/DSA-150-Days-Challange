a9cb8bd44f678a819328be7177e5d94522b6826f
deepmoina2005 <deepmoinaboruah7@gmail.com>
2025-06-02 06:26:13 +0530

    update

--- Day 6 (02-06-2025)/add2Number.cpp
+++ Day 6 (02-06-2025)/add2Number.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <vector> // ✅ Include vector header
 
-using namespace std; // ✅ Use std namespace for vector and cout
+using namespace std; 
 
-/**
- * Definition for singly-linked list.
- */
 struct ListNode {
     int val;
     ListNode *next;
