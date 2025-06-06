58e364d8f2074eac213b2dd3358901858df3aeed
deepmoina2005 <deepmoinaboruah7@gmail.com>
2025-06-06 13:39:32 +0530

    update

---
+++ Day 9 (05-06-2025)/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expandFromCenter(s, i, i);     // Odd-length palindrome
+            int len2 = expandFromCenter(s, i, i + 1); // Even-length palindrome
+            int len = max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substr(start, end - start + 1);
+    }
+
+private:
+    int expandFromCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
+
+// Optional: main function to test the code
+int main() {
+    Solution sol;
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+    string result = sol.longestPalindrome(input);
+    cout << "Longest Palindromic Substring: " << result << endl;
+    return 0;
+}
